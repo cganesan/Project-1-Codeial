55909513c6f365e269aab4c8549203649d090d56
cganesan <cganesan.kc@gmail.com>
2023-08-25 11:05:56 +0530

    first commit-directory setup and express server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const app = express();
+const port = 8000;
+
+
+///*******first type****************
+
+app.listen(port, (err)=>{
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is runnnig on port: ${port}`)
+});
+
+///%%%%%%%%%%%%%Second type%%%%%%%%%%%%%%
+
+// app.listen(port, (err)=>{
+//     if (err){
+//         console.log('Error:', err);
+//     }
+//     console.log('Server is running on port:', port);
+// });
+
+
